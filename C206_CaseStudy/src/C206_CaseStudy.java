a1f7a5aa9f264f95dacea4cbee8a925e36f226b3
DanialIqram <22022260@myrp.edu.sg>
2023-08-03 16:56:47 +0800

    imported helper class, started login feature

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,13 +1,27 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
+	private static User account;
+	private static ArrayList<Student> students;
+	private static ArrayList<Teacher> teachers;
+	private static ArrayList<Activities> activities;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
-		// jannah
-		// lleyton
-		// danial
-		// diya
-
+		account = null;
+		
+		students = new ArrayList<>();
+		students.add(new Student(1, "student", "student@example.com", "student", "1A"));
+		
+		teachers = new ArrayList<>();
+		teachers.add(new Teacher(1, "teacher", "teacher@example.com", "teacher", 1));
+		
+		promptLogin();
+	}
+	
+	public static void promptLogin() {
+		while (account == null) {
+			
+		}
 	}
 
 }
