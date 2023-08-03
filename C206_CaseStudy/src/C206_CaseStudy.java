83cfe9eb5e7511e4662b7f3a17dd1b38d0d46127
DanialIqram <22022260@myrp.edu.sg>
2023-08-03 17:06:39 +0800

    Added login feature

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -20,8 +20,45 @@ public class C206_CaseStudy {
 	
 	public static void promptLogin() {
 		while (account == null) {
+			Helper.line(40, "=");
+			System.out.println("ACCOUNT LOGIN");
+			Helper.line(40, "=");
 			
+			String emailInput = Helper.readString("Enter email: ");
+			String passwordInput = Helper.readString("Enter password: ");
+			
+			for (int i = 0; i < students.size(); i++) {
+				Student student = students.get(i);
+				if (student.getEmail().equalsIgnoreCase(emailInput) && student.getPassword().equals(passwordInput)) {
+					account = student;
+					break;
+				}
+			}
+			
+			for (int i = 0; i < teachers.size(); i++) {
+				Teacher teacher = teachers.get(i);
+				if (teacher.getEmail().equalsIgnoreCase(emailInput) && teacher.getPassword().equals(passwordInput)) {
+					account = teacher;
+					break;
+				}
+			}
+			
+			// Invalid email or password OR no account existing.
+			if (account == null) {
+				System.out.println("Invalid email/password. Try again!");
+			}
+		}
+		
+		System.out.println();
+		Helper.line(40, "=");
+		System.out.println("WELCOME, " + account.getName().toUpperCase());
+		Helper.line(40, "=");
+		System.out.println();
+		
+		if (account.getRole().equals("teacher")) {
+			System.out.println("Logged in Teacher account");
+		} else if (account.getRole().equals("student")) {
+			System.out.println("Logged in Student account");
 		}
 	}
-
 }
