378b3b1484c6ed2ac58e6d82ce26e8dedbe2fca0
reganjonah <132028982+reganjonah@users.noreply.github.com>
2023-08-12 22:18:53 +0800

    Wrote methods in Teacher class

--- C206_CaseStudy/src/Teacher.java
+++ C206_CaseStudy/src/Teacher.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 
 public class Teacher extends User {
 	private int activitiesId;
+	
+	private ArrayList<Student> students; 
+    private ArrayList<Student> pendingStudents;
 
 
 public Teacher(int id, String name, String email, String password, int activitiesId) {
@@ -11,6 +14,14 @@ public Teacher(int id, String name, String email, String password, int activitie
 }
 
 public int getActivitesId() {return activitiesId;}
+
+public ArrayList<Student> getStudents() {
+    return students;
+}
+
+public ArrayList<Student> getPendingStudents() {
+    return pendingStudents;
+}
 
 public String toString() {
 	return"";
