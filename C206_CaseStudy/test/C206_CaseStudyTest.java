e27691500a090b8be67fbfbdbc82ab5b8285501f
diya1256 <22000028@myrp.edu.sg>
2023-08-16 19:15:09 +0800

    test

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -12,11 +12,10 @@ public class C206_CaseStudyTest {
 	private static User admin;
 	private static Student student1;
 	private static Student student2;
-	
-	
+
 	private static Activities activity1;
 	private static Activities activity2;
-	
+
 	private static ArrayList<Student> students;
 	private static ArrayList<Teacher> teachers;
 	private static ArrayList<User> admins;
@@ -29,15 +28,15 @@ public class C206_CaseStudyTest {
 		admin = new User(1, "Admin", "admin@school.com", "admin", "admin");
 		student1 = new Student(1, "Jack", "jack@school.com", "jack", "1A");
 		student2 = new Student(2, "Mary", "mary@school.com", "mary", "1B");
-		
+
 		activity1 = new Activities(1, "Basketball", "Sports", 1);
-		activity2 = new Activities(2, "Hockey", "Sports", 2);
-		
+		activity2 = new Activities(2, "Hockey", "Sports", 1);
+
 		activities = new ArrayList<>();
 		students = new ArrayList<>();
 		teachers = new ArrayList<>();
 		admins = new ArrayList<>();
-		
+
 	}
 
 	@After
@@ -53,47 +52,49 @@ public class C206_CaseStudyTest {
 		admins = null;
 	}
 
-	@Test //@Danial
+	@Test // @Danial
 	public void testDoAddUser() {
-		
+
 	}
-	
-	@Test //@Danial
+
+	@Test // @Danial
 	public void testViewUsers() {
-		
+
 	}
-	
-	@Test //@Danial
+
+	@Test // @Danial
 	public void testDoRemoveUser() {
-		
+
 	}
-	
-	
-	@Test //@Jannah
+
+	@Test // @Jannah
 	public void testDoAddActivity() {
 		activities = new ArrayList<Activities>();
-		//Boundary
+		// Boundary
 		assertNotNull("Check for valid activities to be added to", activities);
 		activities.clear();
 		// Normal - Add an activity to an empty list and the size of the arraylist is 1
 		C206_CaseStudy.doAddActivity(activities, activity1);
-		assertEquals("Test that activity is added into activities ArrayList",1, activities.size());
+		assertEquals("Test that activity is added into activities ArrayList", 1, activities.size());
 		assertSame("Test that acivity added is the same", activities.get(0), activity1);
-		// Normal - Add another activity to test the size of the ArrayList is changed to 2
+		// Normal - Add another activity to test the size of the ArrayList is changed to
+		// 2
 		C206_CaseStudy.doAddActivity(activities, activity2);
-		assertEquals("Test that activity is added into activities ArrayList",2, activities.size());
+		assertEquals("Test that activity is added into activities ArrayList", 2, activities.size());
 		assertSame("Test that acivity added is the same", activities.get(1), activity2);
-		// Error - Adding another activity with the same name, ArrayList should remain as 2
+		// Error - Adding another activity with the same name, ArrayList should remain
+		// as 2
 		C206_CaseStudy.doAddActivity(activities, activity1);
-		assertEquals("Test that activity is added into activities ArrayList",2, activities.size());
+		assertEquals("Test that activity is added into activities ArrayList", 2, activities.size());
 	}
-	
-	@Test //@Jannah
+
+	@Test // @Jannah
 	public void testRetrieveAllActivities() {
-		//Boundary - Check that the activities ArrayList is not null and can be added to
-		assertNotNull("Check for valid ArrayList activities to be added to", activities); 
+		// Boundary - Check that the activities ArrayList is not null and can be added
+		// to
+		assertNotNull("Check for valid ArrayList activities to be added to", activities);
 		assertEquals("Test that ArrayList is empty", 0, activities.size());
-		//Boundary - When list is empty output is shown
+		// Boundary - When list is empty output is shown
 		String allActivities = C206_CaseStudy.retrieveAllActivities(activities);
 		String TestOutput = String.format("| %-20s | %-20s | %-20s | %-20s | %-20s | %-20s |", "ID", "Name", "Category",
 				"No. Of Students", "Max Capacity", "Available");
@@ -104,16 +105,16 @@ public class C206_CaseStudyTest {
 		String allActivities2 = C206_CaseStudy.retrieveAllActivities(activities);
 		String testOutput = String.format("| %-20s | %-20s | %-20s | %-20s | %-20s | %-20s |", "ID", "Name", "Category",
 				"No. Of Students", "Max Capacity", "Available");
-		testOutput += String.format("\n| %-20d | %-20s | %-20s | %-20d | %-20d | %-20s |", 1,"Basketball" , "Sports",
-				0, 1, "Yes");
+		testOutput += String.format("\n| %-20d | %-20s | %-20s | %-20d | %-20d | %-20s |", 1, "Basketball", "Sports", 0,
+				1, "Yes");
 		assertEquals("Test that message is shown when list is empty", testOutput, allActivities2);
 		// Normal - Add another Activity is added and is shown correctly
 		C206_CaseStudy.doAddActivity(activities, activity2);
-		testOutput += String.format("\n| %-20d | %-20s | %-20s | %-20d | %-20d | %-20s |", 2,"Hockey" , "Sports",
-				0, 2, "Yes");
+		testOutput += String.format("\n| %-20d | %-20s | %-20s | %-20d | %-20d | %-20s |", 2, "Hockey", "Sports", 0, 2,
+				"Yes");
 	}
-	
-	@Test //@Jannah
+
+	@Test // @Jannah
 	public void testDoDeleteActivity() {
 		// Boundary
 		assertNotNull("Check for valid ArrayList activities to be added to", activities);
@@ -124,65 +125,79 @@ public class C206_CaseStudyTest {
 		// Error - Delete an activity that is not in the list
 		C206_CaseStudy.doDeleteActivity(activities, 2);
 		assertEquals("Test that ArrayList is still empty", 0, activities.size());
-		
+
 	}
-	
-	@Test //@Regan
+
+	@Test // @Regan
 	public void testDoSetApprovalStatus() {
-		
+
 	}
-	
-	@Test //@Regan
+
+	@Test // @Regan
 	public void testViewAllPending() {
-		
+
 	}
-	
+
 	@Test // @Regan
 	public void testDoDeletePending() {
-		
+
 	}
-	
+
 	@Test // @Lleyton
 	public void testDoAddAttendance() {
 		// Error - Delete an activity that is not in the list
 	}
-	
+
 	@Test // @Lleyton
 	public void testViewAttendance() {
-		
+
 	}
-	
+
 	@Test // @Lleyton
 	public void testDoDeleteAttendance() {
 		// Error - Delete an attendance that is not in the list
 	}
-	
+
 	@Test // @Diya
 	public void testDoRegisterForActivity() {
-		// Check for normal registration successful student registration 
+		// Check for normal registration successful student registration
 		C206_CaseStudy.doRegisterForActivity(student1.getId(), 1);
-		assertEquals(activity1.getPendingStudents().size(),1);
-		
-		//Check boundary successful registration when max capacity is 1 
-		C206_CaseStudy.doRegisterForActivity(student2.getId(),1);
-		assertEquals(activity1.getPendingStudents().size(),1);
-		
-		//Check error when student registers for an invalid activity ID
-		C206_CaseStudy.doRegisterForActivity(student1.getId(),3);
-		assertEquals(activity1.getPendingStudents().size(),1);
-		assertEquals(activity2.getPendingStudents(),0);
-		
+		assertEquals(activity1.getPendingStudents().size(), 1);
+
+		// Check boundary successful registration when max capacity is 1
+		C206_CaseStudy.doRegisterForActivity(student2.getId(), 1);
+		assertEquals(activity1.getPendingStudents().size(), 1);
+
+		// Check error when student registers for an invalid activity ID
+		C206_CaseStudy.doRegisterForActivity(student1.getId(), 3);
+		assertEquals(activity1.getPendingStudents().size(), 1);
+		assertEquals(activity2.getPendingStudents(), 0);
+
 	}
-	
+
 	@Test // @Diya
 	public void testViewAllRegistrations() {
-		//Check for normal view when user press 
+		// Check for normal view when user press
+
 	}
-	
+
 	@Test // @Diya
 	public void testDoDeleteRegistration() {
-		//Check for normal 
+		// Check for normal condition when deleting a registration
+		C206_CaseStudy.doDeleteRegistration(student1.getId(), 1);
+		assertEquals(activity1.getPendingStudents().size(),0);
 		
+		//Check for boundary condition for delete registration. When there isn't any registration 
+		C206_CaseStudy.doDeleteRegistration(student1.getId(),3);
+		assertEquals(activity1.getPendingStudents().size(),0);
+		assertEquals.getStudents().size(),1);
+		
+		//error for delete registration 
+		C206_CaseStudy.doDeleteRegistration(student1.getId(),3);
+		assertEquals(activity1.getPendingStudents(),1);
+		assertEquals(activity2.getPendingStudents(), 0);
+		
+	
 	}
 
 }
